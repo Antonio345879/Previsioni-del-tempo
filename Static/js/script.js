9091b1f84fc7a10fc099f5d28bdf4c99ef487549
Antonio345879 <loconsole.antonio@panettipitagora.edu.it>
2023-09-19 09:51:44 +0200

    previsione.1

---
+++ Static/js/script.js
@@ -0,0 +1,22 @@
+let latitudine = 41.11195395868712
+let longitudine = 16.86029378729626
+
+navigator.geolocation.getCurrentPosition(
+    function (event) {
+        console.log("l'utente ha accettato")
+        console.log(event)
+        latitudine = event.coords.latitude
+        longitudine = event.coords.longitude
+        createMap()
+    },
+
+    function (event) {
+    }
+)
+
+function createMap() {
+    let map = L.map('map').setView([latitudine, longitudine], 10);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+}
